fe30b5e3aeceddb4d79d74488dec51b5a15e5def
Siddhesh <bhosalesiddhesh2000@gmail.com>
2024-04-21 01:00:39 +0530

    fix bugs

--- src/components/Browse/SecondaryContainer.jsx
+++ src/components/Browse/SecondaryContainer.jsx
@@ -9,22 +9,27 @@ import {
   MOVIE_LISTS,
 } from "../../utils/constants";
 import { useSelector } from "react-redux";
+import { Spinner } from "@material-tailwind/react";
 
 const SecondaryContainer = () => {
-  const { shareData: popular } = useFetch(POPULAR);
-  const { shareData: trending } = useFetch(TRENDING);
-  const { shareData: topRated } = useFetch(TOP_RATED);
-  const { shareData: upComing } = useFetch(UPCOMING);
+  const { shareData: popular, loading: popularLoading, error: popularError } = useFetch(POPULAR);
+  const { shareData: trending, loading: trendingLoading, error: trendingError } = useFetch(TRENDING);
+  const { shareData: topRated, loading: topRatedLoading, error: topRatedError } = useFetch(TOP_RATED);
+  const { shareData: upComing, loading: upComingLoading, error: upComingError } = useFetch(UPCOMING);
 
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
-  if (!movies) return;
+  if (!movies || popularLoading || trendingLoading || topRatedLoading || upComingLoading) return <Spinner/>;
+
+  if (popularError || trendingError || topRatedError || upComingError) {
+    return <div>Error: Failed to fetch data.</div>;
+  }
 
   const movieData = [
     { title: MOVIE_LISTS[0], moviesList: movies },
     { title: MOVIE_LISTS[1], moviesList: trending },
     { title: MOVIE_LISTS[2], moviesList: topRated },
     { title: MOVIE_LISTS[3], moviesList: popular },
-    { title: MOVIE_LISTS[4], moviesList: upComing },1
+    { title: MOVIE_LISTS[4], moviesList: upComing },
   ];
 
   return (
